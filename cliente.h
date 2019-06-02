6917476a8e24530b99a97ba0e6fa86bbc4b56957
emiliano pellegrino <emiliano.j.pellegrino@gmail.com>
2019-06-02 14:57:04 -0300

    se agrega struct de cliente, pero  falta implementar lista de creditos

--- cliente.h
+++ cliente.h
@@ -4,5 +4,13 @@
 
 #ifndef TP_FINAL_AYP3_CLIENTE_H
 #define TP_FINAL_AYP3_CLIENTE_H
-
+typedef struct miCliente{
+int id;
+char nombre[100];
+char apellido[100];
+int edad;
+char direccion[100];
+struct miCliente *recomendado;
+//agergar lista de prestamos al struct
+} Cliente;
 #endif //TP_FINAL_AYP3_CLIENTE_H
