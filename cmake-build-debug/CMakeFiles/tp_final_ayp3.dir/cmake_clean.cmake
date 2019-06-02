7a1f9b86d4f2ca4f9c39d2583eb3713ea0627b48
emiliano pellegrino <emiliano.j.pellegrino@gmail.com>
2019-06-02 14:36:52 -0300

    se crea el proyecto

---
+++ cmake-build-debug/CMakeFiles/tp_final_ayp3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp_final_ayp3.dir/main.c.obj"
+  "tp_final_ayp3.pdb"
+  "tp_final_ayp3.exe"
+  "tp_final_ayp3.exe.manifest"
+  "libtp_final_ayp3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tp_final_ayp3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    //Main del proyecto.
+    printf("Hello, World!\n");
+    return 0;
+}
