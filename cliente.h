5229497b8a2b16df1847b331c4865e2ae114d0af
Nahuel Ahumada <NahuelAhumada@users.noreply.github.com>
2019-06-06 23:54:59 -0300

    Metodo nuevo_cliente
    Queda pendiente la validacion de la id

--- cliente.h
+++ cliente.h
@@ -13,4 +13,17 @@ char direccion[100];
 struct miCliente *referido;
 //agergar lista de prestamos al struct
 } Cliente;
+
+//Declaro nuevo cliente, revisar ID
+Cliente nuevo_cliente(int id, char nombre[100],
+char apellido[100], int edad, char direccion[100])
+{
+    Cliente n_cliente;
+    n_cliente= (Cliente) malloc(sizeof(n_cliente));
+    n_cliente.id=id;
+    n_cliente.nombre=nombre;
+    n_cliente.apellido=apellido;
+    n_cliente.edad=edad;
+    n_cliente.direccion=direccion;
+}
 #endif //TP_FINAL_AYP3_CLIENTE_H
