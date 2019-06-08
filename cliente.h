f0d1252f5d490ab655cebc646c759c344ddf8b56
Nahuel Ahumada <NahuelAhumada@users.noreply.github.com>
2019-06-07 23:45:41 -0300

    Nuevo cliente ingresando datos por pantalla
    iniciar_cliente see relaciona con el método nuevo_cliente

--- cliente.h
+++ cliente.h
@@ -25,5 +25,29 @@ char apellido[100], int edad, char direccion[100])
     n_cliente.apellido=apellido;
     n_cliente.edad=edad;
     n_cliente.direccion=direccion;
+    n_cliente.referido=NULL;
+    return n_cliente;
+}
+//Valores por pantalla del nuevo cliente
+Cliente iniciar_cliente()
+{
+    Cliente nuevoCliente;
+    int id;
+    int edad;
+    char nombre[100];
+    char apellido[100];
+    char direccion[100];
+    printf("Ingrese el nombre:\n");
+    scanf("%s",nombre);
+    printf("Ingrese el apellido:\n");
+    scanf("%s",apellido);
+    printf("Ingrese el id de cliente:\n");
+    scanf("%d",&id);
+    printf("Ingrese la edad:\n");
+    scanf("%d",&edad);
+    printf("Ingrese la dirección:\n");
+    scanf("%s",direccion);
+    nuevoCliente=nuevo_cliente(id,nombre,apellido,edad,direccion);
+    return nuevoCliente;
 }
 #endif //TP_FINAL_AYP3_CLIENTE_H
