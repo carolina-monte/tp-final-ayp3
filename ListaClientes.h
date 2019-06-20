040351e7316f25914f8a4361a58eb30ab6f4a883
coki <frassonhector.oscar@gmail.com>
2019-06-20 17:45:55 -0300

    se agrega header con lista de clientes

---
+++ ListaClientes.h
@@ -0,0 +1,16 @@
+//
+// Created by Diez 15-03-19 on 20/06/2019.
+//
+
+#ifndef TP_FINAL_AYP3_LISTACLIENTES_H
+#define TP_FINAL_AYP3_LISTACLIENTES_H
+
+#include "cliente.h"
+typedef struct nodo{
+    Cliente *cliente;
+    struct nodo *sig;
+} *Lista;
+
+Lista getnodo();
+
+#endif //TP_FINAL_AYP3_LISTACLIENTES_H
