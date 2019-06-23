06a37f9220fe2e95c929815323a3ef549955f6cc
Nahuel Ahumada <NahuelAhumada@users.noreply.github.com>
2019-06-22 23:26:14 -0300

    Escritura de archivo
    Siemple iimplementacion inicial de escritura

--- main.c
+++ main.c
@@ -13,12 +13,14 @@ int main() {
 
 int main(){
     Cliente *clientes=NULL;
+    FILE *registro_de_empleados;
     int opcion;
     do{
         printf("1. Agregar cliente\n");
         printf("2. Imprimir clientes\n");
         printf("3. Buscar por nombre\n");
-        printf("4. Salir\n");
+        printf("4. Cargar archivo\n");
+        printf("5. Salir\n");
         scanf("%d",&opcion);
         switch(opcion){
             case 1:
@@ -40,12 +42,29 @@ int main(){
 
             }
                 break;
-            case 4:
+            case 4:{
+                registro_de_empleados=fopen("registro.csv","w");
+                if(registro_de_empleados==NULL){
+                    //En caso de Error
+                    fprintf(stderr,"Error al abrir el registro\n");
+                    exit(1);
+                }
+                //Copia para no perder clientes
+                Cliente *puntero=clientes;
+                while (puntero!=NULL)
+                {
+                    //escritura
+                    fprintf(registro_de_empleados,"%s,%s,%d\n",puntero->nombre,puntero->apellido,puntero->id);
+                    puntero=puntero->referido;
+                }
+            }
+
+            case 5:
                 break;
         }
 
     }
-    while(opcion!=4);
+    while(opcion!=5);
 
     return 0;
 }
